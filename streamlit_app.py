7fefaa170ec8cab2c11e60ef3755a500a0c8ae23
wubowen <wu.bowen.dp@gmail.com>
2025-05-23 17:55:06 +0900

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,6 @@ st.write("This is a test")
 st.write("This is a test")
 
 st.write("This is another test")
+
+
+st.video("https://cospeech-smplx-gesture-videos.s3.ap-northeast-3.amazonaws.com/videos/generated/10_kieks_0_10_10_27.mp4")
