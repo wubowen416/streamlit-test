76d22817f4b9ec4e1ee11c97bc46d5a54f3de75e
wubowen <wu.bowen.dp@gmail.com>
2025-05-23 22:13:48 +0900

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,13 @@ import streamlit as st
 
 st.title("Hello World")
 
-col1, col2 = st.columns(2)
-with col1:
+left, right = st.columns(2)
+with left:
     st.header("Left Video")
     st.video("https://cospeech-smplx-gesture-videos.s3.ap-northeast-3.amazonaws.com/videos/generated/10_kieks_0_10_10_27.mp4")
 
-with col2:
+with right:
     st.header("Right Video")
     st.video("https://cospeech-smplx-gesture-videos.s3.ap-northeast-3.amazonaws.com/videos/generated/10_kieks_0_10_10_27.mp4")
+
+st.radio("Select a video", ["Left Video", "Right Video"], horizontal=True)
