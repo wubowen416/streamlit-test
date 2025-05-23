d909bfc221cc82651658fa82883881d7c40164f2
wubowen <wu.bowen.dp@gmail.com>
2025-05-23 18:03:28 +0900

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,11 @@ import streamlit as st
 
 st.title("Hello World")
 
-st.write("This is a test")
+col1, col2 = st.columns(2)
+with col1:
+    st.header("Left Video")
+    st.video("https://cospeech-smplx-gesture-videos.s3.ap-northeast-3.amazonaws.com/videos/generated/10_kieks_0_10_10_27.mp4")
 
-st.write("This is a test")
-
-st.write("This is a test")
-
-st.write("This is a test")
-
-st.write("This is another test")
-
-
-st.video("https://cospeech-smplx-gesture-videos.s3.ap-northeast-3.amazonaws.com/videos/generated/10_kieks_0_10_10_27.mp4")
+with col2:
+    st.header("Right Video")
+    st.video("https://cospeech-smplx-gesture-videos.s3.ap-northeast-3.amazonaws.com/videos/generated/10_kieks_0_10_10_27.mp4")
