21b24e076b6698a49bc5d122fa701ebb23bbd183
wubowen <wu.bowen.dp@gmail.com>
2025-05-23 22:20:32 +0900

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,11 @@ st.title("Hello World")
 
 left, right = st.columns(2, border=True)
 with left:
-    st.header("Left", divider=True)
+    st.header("Left")
     st.video("https://cospeech-smplx-gesture-videos.s3.ap-northeast-3.amazonaws.com/videos/generated/10_kieks_0_10_10_27.mp4")
 
 with right:
-    st.header("Right", divider=True)
+    st.header("Right")
     st.video("https://cospeech-smplx-gesture-videos.s3.ap-northeast-3.amazonaws.com/videos/generated/10_kieks_0_10_10_27.mp4")
 
 st.radio(
