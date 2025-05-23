71bb5b6e7edaa4cca8981520b669b1a21af6a010
wubowen <wu.bowen.dp@gmail.com>
2025-05-23 22:19:42 +0900

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 st.title("Hello World")
 
-left, right = st.columns(2)
+left, right = st.columns(2, border=True)
 with left:
-    st.header("Left")
+    st.header("Left", divider=True)
     st.video("https://cospeech-smplx-gesture-videos.s3.ap-northeast-3.amazonaws.com/videos/generated/10_kieks_0_10_10_27.mp4")
 
 with right:
