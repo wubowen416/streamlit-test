99957b878c3f7fcea0d3ccdd8ff373d5ec9a1594
wubowen <wu.bowen.dp@gmail.com>
2025-05-23 22:21:13 +0900

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,14 @@ with right:
     st.header("Right")
     st.video("https://cospeech-smplx-gesture-videos.s3.ap-northeast-3.amazonaws.com/videos/generated/10_kieks_0_10_10_27.mp4")
 
-st.radio(
+seletion = st.radio(
     label="Select a video", 
     options=["Left", "Equal", "Right"]
 )
+
+if seletion == "Left":
+    st.write("Left is selected")
+elif seletion == "Equal":
+    st.write("Equal is selected")
+else:
+    st.write("Right is selected")
