ce96b7885e7289b9e71992d9b38e2c51c3c25e8a
wubowen <wu.bowen.dp@gmail.com>
2025-05-23 22:18:04 +0900

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,4 @@ with right:
     st.header("Right Video")
     st.video("https://cospeech-smplx-gesture-videos.s3.ap-northeast-3.amazonaws.com/videos/generated/10_kieks_0_10_10_27.mp4")
 
-st.radio("Select a video", ["Left Video", "Right Video"], horizontal=True)
+st.radio("Select a video", ["Left Video", "Right Video"])
